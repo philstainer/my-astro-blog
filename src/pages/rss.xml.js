77e6fba0f677d558d266a5835d11d5ecba7dd9b4
Phil Stainer <philstainer.dev@gmail.com>
2022-09-11 12:28:38 +0100

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,18 @@ import rss from '@astrojs/rss';
 
 import { SITE_DESCRIPTION, SITE_TITLE } from '../config';
 
+const postImportResult = import.meta.glob('./blog/**/*.{md,mdx}', {
+  eager: true,
+});
+const posts = Object.values(postImportResult);
+
 export const get = () =>
   rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./blog/**/*.{md,mdx}'),
+    items: posts.map((post) => ({
+      ...post,
+      pubDate: post.frontmatter.pubDate,
+    })),
   });
